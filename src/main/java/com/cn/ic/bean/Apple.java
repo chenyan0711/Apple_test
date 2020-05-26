7eca3bcbb6aaf4baba228870b809942c81be73d5
root <chen_0711@sina.cn>
2020-05-26 15:22:14 +0800

    init demo

---
+++ src/main/java/com/cn/ic/bean/Apple.java
@@ -0,0 +1,10 @@
+package com.cn.ic.bean;
+
+public class Apple {
+	private String AppName = "good apple ||| jenkins |||heloo 12345 1111 asdasd 123123";
+
+	public String getAppName() {
+		return AppName;
+	}
+
+}
